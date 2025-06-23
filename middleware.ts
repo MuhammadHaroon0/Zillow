6838839e3459014bf8a133bc7fb3dadb05bd0b33
emp001-algomighty <anshkakkar@algomighty.com>
2025-06-24 01:32:37 +0530

    added cors for api

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,52 @@
 import { NextRequest, NextResponse } from "next/server";
 
+const corsOptions: {
+  allowedMethods: string[];
+  allowedOrigins: string[];
+  allowedHeaders: string;
+  exposedHeaders: string;
+  maxAge?: number;
+  credentials: boolean;
+} = {
+  allowedMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedOrigins: process.env?.ALLOWED_ORIGIN?.split(",") || [],
+  allowedHeaders: process.env?.ALLOWED_HEADERS || "",
+  exposedHeaders: process.env?.EXPOSED_HEADERS || "",
+  maxAge: (process.env?.MAX_AGE && parseInt(process.env?.MAX_AGE)) || undefined,
+  credentials: process.env?.CREDENTIALS == "true",
+};
+
 export function middleware(req: NextRequest) {
+  const response = NextResponse.next();
+
+  const origin = req.headers.get("origin") ?? "";
+
+  if (corsOptions.allowedOrigins.includes(origin)) {
+    response.headers.set("Access-Control-Allow-Origin", origin);
+    return response;
+  }
+
+  response.headers.set(
+    "Access-Control-Allow-Credentials",
+    corsOptions.credentials.toString()
+  );
+  response.headers.set(
+    "Access-Control-Allow-Methods",
+    corsOptions.allowedMethods.join(",")
+  );
+  response.headers.set(
+    "Access-Control-Allow-Headers",
+    corsOptions.allowedHeaders
+  );
+  response.headers.set(
+    "Access-Control-Expose-Headers",
+    corsOptions.exposedHeaders
+  );
+  response.headers.set(
+    "Access-Control-Max-Age",
+    corsOptions.maxAge?.toString() ?? ""
+  );
+
   const { pathname } = req.nextUrl;
   const token = req.cookies.get("token")?.value;
 
@@ -15,9 +61,9 @@ export function middleware(req: NextRequest) {
     url.pathname = "/login";
     return NextResponse.redirect(url);
   }
-  return NextResponse.next();
+  return response;
 }
 
 export const config = {
-  matcher: ["/:path*"],
+  matcher: ["/:path*", "/api/:path*"],
 };
